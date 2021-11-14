a9e7bb30023bb50ed2fd7df4fd9a5027c45ad67a
Emil Johannesson <emil.johannesson@utb.ecutbildning.se>
2021-11-14 18:43:48 +0100

    first commit

---
+++ style.css
@@ -0,0 +1,9 @@
+/*
+Theme Name: EmilJProjekt
+Text Domain: EmilJProjekt
+Version: 0.1
+Description: Projektarbete CMS
+Tags: 
+Author: Emil Johannesson
+Author URI: https://github.com/EmilJohannesson
+*/
